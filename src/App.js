a40d677e9ec042b25643daeedc673a7fe7f3353c
Exodus <hasinmansoor90@gmail.com>
2025-05-25 17:38:14 +0500

    gtghti

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ function App() {
     <div className="App">
       {/* Navbar */}
       <header className="navbar">
-        <h2>My React App edit 10</h2>
+        <h2>My React App - Working</h2>
         <nav>
           <a href="/">Home</a>
           <a href="/">About</a>
