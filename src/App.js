b7857aca4a45382960bb47a0dd6688b170e80b86
Exodus <119557482+ExodusO1@users.noreply.github.com>
2025-06-01 10:29:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function App() {
     <div className="App">
       {/* Navbar */}
       <header className="navbar">
-        <h2>My React App - Working</h2>
+        <h2>My React App - Working01</h2>
         <nav>
           <a href="/">Home</a>
           <a href="/">About</a>
